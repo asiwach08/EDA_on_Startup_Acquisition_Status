03e6972f484785cf7fa2d3d6be926d50d167b9d3
Manthan Kaundal <manthan.kaundal@velomatch.in>
2023-01-10 16:15:49 +0530

    demo.py 1

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+
+def func():
+    counts = 0
+    numbers = int(input())
+    for i in range(numbers):
+        if i//2 == 0:
+            print("ok")
+        else:
+            print('not ok')
+            counts += 1
+    return counts
